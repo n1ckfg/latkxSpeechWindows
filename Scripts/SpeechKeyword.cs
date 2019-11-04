b449a184db940c263b134cf2d1520ecb2793a281
Nick Fox-Gieg <nick@fox-gieg.com>
2019-11-03 20:16:43 -0500

    first

---
+++ Scripts/SpeechKeyword.cs
@@ -0,0 +1,64 @@
+﻿// https://developer.microsoft.com/en-us/windows/mixed-reality/voice_input_in_unity
+// Keyword recognition is fastest and easiest if you know the commands you need in advance.
+// It's available in Windows 7, 8, 10, and on UWP devices like HoloLens.
+// It doesn't require internet access or any special permissions.
+// It's designed to run continually, listening for keywords.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class SpeechKeyword : MonoBehaviour {
+
+    public enum ConfidenceLevel { HIGH, MEDIUM, LOW };
+    public ConfidenceLevel confidenceLevel = ConfidenceLevel.MEDIUM;
+    public AudioSource sound;
+    public bool setupKeywordsOnStart = false;
+
+    [HideInInspector] public Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    private KeywordRecognizer keywordRecognizer;
+    
+    private void Start() {
+        if (setupKeywordsOnStart) setupKeywords();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        System.Action keywordAction;
+
+        sound.Play();
+
+        Debug.Log("Heard keyword: \"" + args.text + "\"");
+
+        if (keywords.TryGetValue(args.text, out keywordAction)) { // if the keyword recognized is in our dictionary, call that Action.
+            keywordAction.Invoke();
+        }
+    }
+
+    public void setupKeywords() {
+        keywords.Clear();
+
+        keywords.Add("red", () => { // Create keyword for keyword recognizer
+            // action to be performed when this keyword is spoken
+        });
+
+        init();
+    }
+
+    public void init() {
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray(), (UnityEngine.Windows.Speech.ConfidenceLevel) confidenceLevel);
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        startListening();
+    }
+
+    public void startListening() {
+        keywordRecognizer.Start();
+    }
+
+    public void stopListening() {
+        keywordRecognizer.Stop();
+    }
+
+}
---
+++ Scripts/SpeechKeyword_ColorChanger.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechKeyword_ColorChanger : MonoBehaviour
+{
+
+    public Renderer ren;
+
+    private SpeechKeyword speech;
+    private Material mat;
+
+    private void Awake() {
+        speech = GetComponent<SpeechKeyword>();
+        mat = ren.sharedMaterial;
+    }
+
+    private void Start() {
+        mat.SetColor("_Color", Color.gray);
+
+        setupKeywords();
+    }
+
+    private void setupKeywords() {
+        speech.keywords.Clear();
+
+        speech.keywords.Add("red", () => {
+            mat.SetColor("_Color", Color.red);
+        });
+
+        speech.keywords.Add("green", () => {
+            mat.SetColor("_Color", Color.green);
+        });
+
+        speech.keywords.Add("blue", () => {
+            mat.SetColor("_Color", Color.blue);
+        });
+
+        speech.keywords.Add("orange", () => {
+            mat.SetColor("_Color", new Color(1f, 0.5f, 0f));
+        });
+
+        speech.keywords.Add("yellow", () => {
+            mat.SetColor("_Color", Color.yellow);
+        });
+
+        speech.keywords.Add("pink", () => {
+            mat.SetColor("_Color", Color.magenta);
+        });
+
+        speech.keywords.Add("magenta", () => {
+            mat.SetColor("_Color", Color.magenta);
+        });
+
+        speech.keywords.Add("purple", () => {
+            mat.SetColor("_Color", new Color(0.5f, 0f, 1f));
+        });
+
+        speech.keywords.Add("teal", () => {
+            mat.SetColor("_Color", Color.cyan);
+        });
+
+        speech.keywords.Add("cyan", () => {
+            mat.SetColor("_Color", Color.cyan);
+        });
+
+        speech.keywords.Add("white", () => {
+            mat.SetColor("_Color", Color.white);
+        });
+
+        speech.keywords.Add("black", () => {
+            mat.SetColor("_Color", Color.black);
+        });
+
+        speech.keywords.Add("gray", () => {
+            mat.SetColor("_Color", Color.gray);
+        });
+
+        speech.init();
+    }
+
+}
